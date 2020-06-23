fcbb35eb1e3e3f4f73b4489d0d4980640386d345
Carlos López Quintanilla <carlos.lopez@psig.es>
2020-06-23 11:51:01 +0200

    Update expresiones.py

--- expresiones.py
+++ expresiones.py
@@ -1,4 +1,12 @@
 
-
+# Para añadir imagen 
 @project_folder  || '/../Dades/Fanals/Fotos/'
  ||  "id_quadre"  || '.jpg'
+
+ 
+ # Para añadir html con imagen
+ <style>
+* {margin: 0px; padding: 0px}
+</style>
+<img src="[%  'file:///'  || @project_folder || '/../Dades/Fanals/Fotos/' || "id_quadre" ||  '.jpg' %]" 
+         width="400" height="300" />
